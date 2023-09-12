e88d04f12105f60c58f268e31f37cc5f6dcb306e
CodeSandbox <support@codesandbox.io>
2023-09-12 08:20:23 +0000

    Sandbox snapshot commit

---
+++ context/useGlobalContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, ReactNode, useEffect, useState,useContext } from "react";
+
+type AppProviderProps = {
+    children:ReactNode;
+}
+
+type contextValue = {
+    topRated: Array<any>
+}
+
+const AppContext = createContext({})
+
+const AppProvider = ({children} : AppProviderProps)=>{
+    const [topRated, setTopRated] = useState([]);
+
+    const fetchTopRated = async () =>{
+        try {
+            const response = await fetch("https://api.themoviedb.org/3/account/12668959/rated/movies?language=en-US&page=1&sort_by=created_at.asc");
+            const data = await response.json();
+            setTopRated(data);
+        } catch (error) {
+            console.log("data failed to fetch");
+        }
+    }
+
+    console.log(topRated);
+
+    useEffect(() =>{
+        fetchTopRated();
+    }, [])
+
+    const contextValue:contextValue = {
+        topRated,
+    }
+
+    return (
+        <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+  
+  export { AppContext, AppProvider };
