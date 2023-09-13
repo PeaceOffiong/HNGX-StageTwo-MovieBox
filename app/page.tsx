cf01514e8cc84206bce81e0dbcaac09966086c69
Peace <97194474+PeaceOffiong@users.noreply.github.com>
2023-09-13 11:34:42 +0000

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
+import { useGlobalContext } from "./context/useGlobalContext";
+
 
 export default function Home() {
+  const { topRated } = useGlobalContext();
   return (
-      <main>
-        hey there
-      </main>
-  )
+    <main>
+      <nav>
+        <div>
+          <Image src="./tv.svg" alt="logo" height="100" width="100" />
+          <h2>Movie Box</h2>
+        </div>
+        <div>
+          <input type="text" />
+          <BsSearch/>
+        </div>
+        <div></div>
+      </nav>
+      <section></section>
+    </main>
+  );
 }
